f402468ca75252de4d1746562baed80096ab52bd
Benedict Belz <info@benedictbelz.eu>
2021-09-18 16:59:37 +0200

    5.3: Real-Time Applications & Data Storage

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import firebase from 'firebase';
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
 	constructor(props) {
@@ -8,60 +10,113 @@ export default class Chat extends React.Component {
 
 		this.state = {
 			messages: [],
+			uid: 0,
+			user: {
+				_id: '',
+				name: ''
+			}
 		};
+
+		const firebaseConfig = {
+			apiKey: 'AIzaSyDMAytTWCgYVtCa0wSDP_iNtPMXnIWMas4',
+			authDomain: 'chat-app-40a4c.firebaseapp.com',
+			projectId: 'chat-app-40a4c',
+			storageBucket: 'chat-app-40a4c.appspot.com',
+			messagingSenderId: '911661793973',
+			appId: '1:911661793973:web:e3b0e362ecaeb2fbf0b4fc',
+			measurementId: 'G-N0Q50QLVYS'
+		};
+
+		if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+		this.referenceChatMessages = firebase.firestore().collection('messages');
+		this.referenceMessageUser = null;
 	}
 
 	componentDidMount() {
 		// SET OPTIONS
-		let { name } = this.props.route.params;
+		const { name } = this.props.route.params;
 		this.props.navigation.setOptions({ title: name });
-
-		this.setState({ messages: [{
-				_id: 1,
-				text: 'Hello ' + name,
-				createdAt: new Date(),
+		// AUTHENTICATION
+		this.authUnsubscribe = firebase.auth().onAuthStateChanged(user => {
+			if (!user) firebase.auth().signInAnonymously();
+			
+			this.setState({
+				uid: user.uid,
+				messages: [],
 				user: {
-					_id: 2,
-					name: 'React Native',
-					avatar: 'https://placeimg.com/140/140/any',
-				}
-			},
-			{
-				_id: 2,
-				text: 'Welcome to Chat App',
-				createdAt: new Date(),
-				system: true,
-			}
-		]});
+					_id: user.uid,
+					name: name,
+			  	}
+			});
+
+			this.referenceMessagesUser = firebase
+				.firestore()
+				.collection('messages')
+				.where('uid', '==', this.state.uid);
+			this.unsubscribe = this.referenceChatMessages
+				.orderBy('createdAt', 'desc')
+				.onSnapshot(this.onCollectionUpdate);
+		});
+	}
+
+	componentWillUnmount() {
+		this.authUnsubscribe();
+	}
+
+	addMessages() {
+		const message = this.state.messages[0];
+		this.referenceChatMessages.add({
+			uid: this.state.uid,
+			_id: message._id,
+			createdAt: message.createdAt,
+			text: message.text,
+			user: message.user,
+		});
+	}
+
+	onCollectionUpdate = querySnapshot => {
+		const messages = [];
+		querySnapshot.forEach(document => {
+		  	let data = document.data();
+		  	messages.push({
+				_id: data._id,
+				createdAt: data.createdAt.toDate(),
+				text: data.text,
+				user: {
+			  		_id: data.user._id,
+			  		name: data.user.name,
+				},
+		  	});
+		});
+		this.setState({ messages });
 	}
 
 	onSend(messages = []) {
 		this.setState(previousState => ({
 			messages: GiftedChat.append(previousState.messages, messages),
-		}))
+		}), () => this.addMessages());
 	}
-
+	
 	renderBubble(props) {
 		return <Bubble {...props} wrapperStyle={{ 
-			right: { backgroundColor: '#000' }
+			//left: { backgroundColor: '#2F2F2F' },
+			right: { backgroundColor: '#000000' }
 		}} />;
 	}
 
 	render() {
-		let { color } = this.props.route.params;
+		const { color } = this.props.route.params;
 
 		return (
 			<View style={[styles.container, {backgroundColor: color}]}>
 				<GiftedChat
-					renderBubble={this.renderBubble.bind(this)}
+					isTyping={true}
 					messages={this.state.messages}
 					onSend={messages => this.onSend(messages)}
-					user={{ _id: 1 }}
+					renderBubble={this.renderBubble.bind(this)}
+					user={this.state.user}
 				/>
-				{Platform.OS === 'android'
-					? <KeyboardAvoidingView behavior='height' />
-					: null
-				}
+				{Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
 			</View>
 		);
 	}
@@ -71,10 +126,5 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: 'center',
-	},
-	text: {
-		fontSize: 16,
-		color: '#FFFFFF',
-		textAlign: 'center',
-	},
+	}
 });
