f9397592330717484392de534c0734c7fec01f07
Benedict Belz <info@benedictbelz.eu>
2021-09-22 22:23:21 +0200

    5.4: Storing Data on the Client Side

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import NetInfo from '@react-native-community/netinfo';
 import firebase from 'firebase';
 require('firebase/firestore');
 
@@ -14,7 +16,8 @@ export default class Chat extends React.Component {
 			user: {
 				_id: '',
 				name: ''
-			}
+			},
+			connection: false
 		};
 
 		const firebaseConfig = {
@@ -36,31 +39,72 @@ export default class Chat extends React.Component {
 		// SET OPTIONS
 		const { name } = this.props.route.params;
 		this.props.navigation.setOptions({ title: name });
-		// AUTHENTICATION
-		this.authUnsubscribe = firebase.auth().onAuthStateChanged(user => {
-			if (!user) firebase.auth().signInAnonymously();
-			
-			this.setState({
-				uid: user.uid,
-				messages: [],
-				user: {
-					_id: user.uid,
-					name: name,
-			  	}
-			});
 
-			this.referenceMessagesUser = firebase
-				.firestore()
-				.collection('messages')
-				.where('uid', '==', this.state.uid);
-			this.unsubscribe = this.referenceChatMessages
-				.orderBy('createdAt', 'desc')
-				.onSnapshot(this.onCollectionUpdate);
+		NetInfo.fetch().then(connection => { 
+			if (connection.isConnected) {
+				// CONNECTION ONLINE
+			  	this.setState({ connection: true });
+				// AUTHENTICATION
+				this.authUnsubscribe = firebase.auth().onAuthStateChanged(user => {
+					if (!user) firebase.auth().signInAnonymously();
+					
+					this.setState({
+						uid: user.uid,
+						messages: [],
+						user: {
+							_id: user.uid,
+							name: name,
+						}
+					});
+
+					this.referenceMessagesUser = firebase
+						.firestore()
+						.collection('messages')
+						.where('uid', '==', this.state.uid);
+					this.unsubscribe = this.referenceChatMessages
+						.orderBy('createdAt', 'desc')
+						.onSnapshot(this.onCollectionUpdate);
+				});
+			}
+			else {
+				// CONNECTION OFFLINE
+				this.setState({ connection: false });
+				this.getMessages();
+			}
 		});
 	}
 
 	componentWillUnmount() {
 		this.authUnsubscribe();
+	}
+
+	async getMessages() {
+		try {
+			const messages = await AsyncStorage.getItem('messages') || [];
+			this.setState({ messages: JSON.parse(messages) });
+		}
+		catch(error) {
+			console.error(error.message);
+		}
+	}
+
+	async deleteMessages() {
+		try {
+			await AsyncStorage.removeItem('messages');
+			this.setState({ messages: [] });
+		}
+		catch(error) {
+			console.error(error.message);
+		}
+	}
+
+	async saveMessages() {
+		try {
+			await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
+		}
+		catch(error) {
+			console.error(error.message);
+		}
 	}
 
 	addMessages() {
@@ -94,25 +138,35 @@ export default class Chat extends React.Component {
 	onSend(messages = []) {
 		this.setState(previousState => ({
 			messages: GiftedChat.append(previousState.messages, messages),
-		}), () => this.addMessages());
+		}), () => {
+			this.addMessages();
+			this.saveMessages();
+		});
 	}
 	
 	renderBubble(props) {
 		return <Bubble {...props} wrapperStyle={{ 
-			//left: { backgroundColor: '#2F2F2F' },
 			right: { backgroundColor: '#000000' }
 		}} />;
 	}
 
+	renderInputToolbar(props) {  
+		if (this.state.connection === false) {
+
+		}
+		else
+		  	return <InputToolbar {...props}/>;
+	}
+
 	render() {
 		const { color } = this.props.route.params;
-
 		return (
 			<View style={[styles.container, {backgroundColor: color}]}>
 				<GiftedChat
 					isTyping={true}
 					messages={this.state.messages}
 					onSend={messages => this.onSend(messages)}
+					renderInputToolbar={this.renderInputToolbar.bind(this)}
 					renderBubble={this.renderBubble.bind(this)}
 					user={this.state.user}
 				/>
